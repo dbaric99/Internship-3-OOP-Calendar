e2f1c2aca13bc533ecc4249153b7a8e557021bd9
Damjana Baric <damjana.baric1999@gmail.com>
2022-11-26 13:52:53 +0100

    Defining classes and their properties

---
+++ Calendar/Classes/Event.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Calendar.Classes
+{
+	public class Event
+	{
+        public Guid Id { get; }
+		public string EventName { get; set; }
+		public string Location { get; set; }
+		public DateTime StartDate { get; set; }
+		public DateTime EndDate { get; set; }
+		public List<string> Participants { get; private set; }
+
+		public Event()
+		{
+			Id = Guid.NewGuid();
+		}
+
+		public Event(string eventName, string location, DateTime startDate, DateTime endDate, List<string> participants)
+		{
+			Id = Guid.NewGuid();
+			EventName = eventName;
+			Location = location;
+			StartDate = startDate;
+			EndDate = endDate;
+			Participants = participants;
+		}
+	}
+}
+
---
+++ Calendar/Classes/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Calendar.Classes
+{
+	public class Person
+	{
+		public string Name { get; set; }
+		public string Surname { get; }
+		public string Email { get; }
+		public Dictionary<Guid, bool> Attendance { get; private set; }
+
+		public Person(string surname, string email)
+		{
+			Surname = surname;
+			Email = email;
+		}
+
+		public Person(string name, string surname, string email, Dictionary<Guid, bool> attendance)
+		{
+			Name = name;
+			Surname = surname;
+			Email = email;
+			Attendance = attendance;
+		}
+	}
+}
+
